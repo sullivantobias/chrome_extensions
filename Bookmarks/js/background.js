2a8b69f8c8f620afcb8c1dac237eaab89bbb53e6
Sullivan Tobias <githubst@gmail.com>
2019-04-17 01:59:04 +0200

    Adding Remove Action

--- Bookmarks/js/background.js
+++ Bookmarks/js/background.js
@@ -3,7 +3,22 @@
  */
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.bookmarks.getTree( bookmarks => {
-        chrome.storage.sync.set({bookmarks: bookmarks[0].children[0].children.length});
-    });
+  updateBookmarks();
+  chrome.bookmarks.onCreated.addListener(() => {
+    updateBookmarks();
+  });
+  chrome.bookmarks.onRemoved.addListener(() => {
+    updateBookmarks();
+  });
 });
+
+const updateBookmarks = () => {
+  chrome.bookmarks.getTree(bookmarks => {
+    chrome.storage.sync.set({
+      bookmarks: {
+        count: bookmarks[0].children[0].children.length,
+        title: bookmarks[0].children[0].children
+      }
+    })
+  });
+};
--- Bookmarks/js/popup.js
+++ Bookmarks/js/popup.js
@@ -1,8 +1,37 @@
 /**
  * Popup interaction
  */
-
 chrome.storage.sync.get('bookmarks', data => {
-    let contentTabs = document.querySelector('#bookmarks');
-    contentTabs.innerHTML = `Bookmarks: ${data.bookmarks}`;
+  fillContent(data)
 });
+
+const removeBookMark = (id) => {
+  document.querySelectorAll('.remove').forEach((item) => {
+    item.addEventListener('click', () => {
+      chrome.bookmarks.remove(id);
+      if (id === item.getAttribute('data-id')) {
+        item.parentElement.remove();
+      }
+    })
+  });
+};
+
+const fillContent = data => {
+  let contentTabs = document.querySelector('#bookmarks');
+  let title = document.querySelector('#title');
+  contentTabs.innerHTML = `Bookmarks: ${data.bookmarks.count}`;
+
+  for (let test in data.bookmarks.title) {
+    const li = document.createElement('li');
+    const target = document.createElement('button');
+    target.classList.add('remove');
+    target.innerHTML = 'Remove';
+    target.setAttribute('data-id', data.bookmarks.title[test].id);
+    li.innerText = data.bookmarks.title[test].title;
+    title.appendChild(li);
+    li.appendChild(target);
+
+    removeBookMark(data.bookmarks.title[test].id);
+  }
+
+};
